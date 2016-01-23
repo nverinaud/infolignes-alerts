68accaa3904bd4cc04a1646c2991e05f152964c7
Nicolas VERINAUD <n.verinaud@gmail.com>
2016-01-23 17:23:55 +0100

    Add fsharp project.

---
+++ InfolignesAlerts/InfolignesAlerts/AssemblyInfo.fs
@@ -0,0 +1,26 @@
+﻿//
+// Author:
+//     Nicolas VERINAUD <nicolas@chloro.eu>
+//
+// Copyright (c) 2016 Chlorophyll Vision. All Rights Reserved.
+//
+namespace InfolignesAlerts
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("InfolignesAlerts")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Chlorophyll Vision")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Chlorophyll Vision")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ InfolignesAlerts/InfolignesAlerts/Program.fs
@@ -0,0 +1,44 @@
+﻿//
+// Author:
+//     Nicolas VERINAUD <n.verinaud@gmail.com>
+//
+// Copyright (c) 2016 Nicolas VERINAUD. All Rights Reserved.
+//
+
+open System
+open System.Net
+open System.Collections.Specialized
+open FSharp.Data
+
+// 1. POST http://www.sncf.com/fr/itineraire with params
+// 2. Get the cookies
+// 3. GET http://www.sncf.com/fr/horaires-info-trafic/trajet/resultats with the cookies
+
+let f =
+    let cookieContainer = CookieContainer()
+    let url = "http://www.sncf.com/fr/itineraire"
+    Http.Request (
+        url, 
+        query = [
+            "datemax", ""; 
+            "sncfdirect", ""; 
+            "gareOrigne", "Mutzig"; 
+            "codeOrigine", ""; 
+            "gareDestination", "Strasbourg";
+            "codeDestination", "";
+            "date", "25/01/2016";
+            "timeSettings", "0";
+            "heure", "0730";
+            "gareVia", "";
+            "codeVia", ""],
+        cookieContainer = cookieContainer,
+        httpMethod = "post"
+    ) |> ignore
+
+    let response = Http.RequestString ("http://www.sncf.com/fr/horaires-info-trafic/trajet/resultats", cookieContainer = cookieContainer)
+    response
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%s" f
+    0
